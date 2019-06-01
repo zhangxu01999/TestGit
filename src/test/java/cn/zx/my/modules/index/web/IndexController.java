85a754c95d135f1851a613faa5e9cafe72390a9e
HubsSky <zx15662007677@gmail.com>
2019-06-01 14:24:58 +0800

    three

--- src/test/java/cn/zx/my/modules/index/web/IndexController.java
+++ src/test/java/cn/zx/my/modules/index/web/IndexController.java
@@ -1,4 +1,9 @@
 package cn.zx.my.modules.index.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class IndexController {
 
